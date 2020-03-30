86e66d08d02422ba1c957618d92514a9b30f9283
hansungj <43443895+hansungj@users.noreply.github.com>
2020-03-30 16:57:40 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,10 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch import optim
+
 
 import numpy as np
 import random
-
+from Optimizer import *
+from BatchLoader import *
+from DataProcessing import *
 
 class EmbeddingLayer(nn.Module):
 
@@ -28,18 +32,20 @@ class EmbeddingLayer(nn.Module):
 
 class CharCNN(nn.Module):
 
-	def __init__(self, embedding_dim, kernels = [2,3,4,5,6,7], num_filters = 25):
+	def __init__(self, embedding_dim, kernels = [2,3,4,5,6,7], num_filters = [2,3,4,5,6,7]):
 
 		'''
 		This module will take a list of "n-grams" = "filter widths"
 		and create sum of N filters that will be concatenated 
 
+		The difference in output sizes of different CNN filters
+		are okay because we will be applying global pooling at the end
+
 		'''
 
 		super(CharCNN, self).__init__()
-		self.num_filters = sum([kern*num_filters for kern in kernels])
-
-		self.kernels = nn.ModuleList([nn.Conv1d( embedding_dim, kernel*num_filters, kernel , stride = 1) for kernel in kernels])
+		self.num_filters = sum(num_filter for num_filter in num_filters)
+		self.kernels = nn.ModuleList([nn.Conv2d(1 , num_filter , (kernel, embedding_dim) , stride = (1,1)) for kernel, num_filter in zip(kernels, num_filters)])
 
 	def forward(self, x):
 
@@ -48,10 +54,10 @@ class CharCNN(nn.Module):
 		output: [batch_size x temporal x num_filter]
 	
 		'''
-		x = [ F.relu(kernel(x)) for kernel in self.kernels]
+		x = [ F.relu(kernel(x).squeeze()) for kernel in self.kernels]
 		x = [ F.max_pool1d(kernel, kernel.size()[-1]) for kernel in x]
 		x = torch.cat(x, dim = 1 )
-		return torch.squeeze(x)
+		return x
 
 
 class HighwayNetwork(nn.Module):
@@ -111,6 +117,7 @@ class Classifier(nn.Module):
 		self.FFN = nn.ModuleList([nn.Linear(input_dim, d_ff)])
 		self.FFN.extend([nn.Linear(d_ff, d_ff) for _ in range(num_ff_layers-1)])
 		self.FFN.append(nn.Linear(d_ff, word_vocab_size))
+		
 		self.Sublayer = nn.ModuleList([SubLayer(d_ff, dropout) for _ in range(num_ff_layers)])
 		self.Out = nn.LogSoftmax(dim=-1) #word_vocab_size
 
@@ -126,15 +133,16 @@ class Classifier(nn.Module):
 
 class CharacterRNNLM(nn.Module):
 
-	def __init__(self, embedding_dim, vocab_size, word_vocab_size, 
+	def __init__(self, max_w_length, embedding_dim, vocab_size, word_vocab_size, 
 						padding_idx = 0, kernels = [2,3,4,5,6,7], 
 						num_filters = 100, rnn_type = 'LSTM', num_layers = 3,
-						 d_rnn = 200, d_ff = 500, dropout = 0.5, bidirectional = False ):
+						 d_rnn = 200, d_ff = 500, dropout = 0.5, bidirectional = False, jointly_train = False):
 
 
 
 		super(CharacterRNNLM, self).__init__()
 		self.bidirectional = bidirectional
+		self.jointly_train = jointly_train
 		self.d_rnn = d_rnn
 
 		self.EmbeddingLayer = EmbeddingLayer(embedding_dim, vocab_size, padding_idx)
@@ -145,8 +153,9 @@ class CharacterRNNLM(nn.Module):
 
 		self.RNNLM = RNNLM(rnn_type, self.word_dim, num_layers, d_rnn, dropout, bidirectional)
 
-		if bidirectional:
+		if bidirectional and not jointly_train:
 			d_rnn *= 2
+
 		self.Classifier = Classifier(word_vocab_size, d_ff, d_rnn)
 
 	def forward(self, x, debug = False):
@@ -154,9 +163,10 @@ class CharacterRNNLM(nn.Module):
 		x = self.EmbeddingLayer(x)
 
 		batch_size, seq_len, mac_c_len, emb_dim = x.size()
-		x = x.view(batch_size * seq_len , emb_dim, mac_c_len)
+		x = x.view(batch_size * seq_len , 1, mac_c_len, emb_dim)
 
 		y = self.CharCNN(x)
+		y = y.view(batch_size,  seq_len, -1)
 		z = self.HighwayNetwork(y)
 
 		z = z.view(seq_len, batch_size, -1)
@@ -164,7 +174,15 @@ class CharacterRNNLM(nn.Module):
 
 		if self.bidirectional:
 			f_z, b_z = z[:-2,:,:self.d_rnn], z[2:,:,self.d_rnn:]
-			z = torch.cat((f_z,b_z), dim = -1)
+
+			if self.jointly_train:
+				f_z = f_z.view(f_z.size()[1], f_z.size()[0], -1)
+				b_z = b_z.view(b_z.size()[1], b_z.size()[0], -1)
+				return self.Classifier(f_z), self.Classifier(b_z)
+
+			else:
+				z = torch.cat((f_z,b_z), dim = -1)
+
 		else:
 			z = z[:-2,:,:]
 

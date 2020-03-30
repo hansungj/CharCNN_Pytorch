356948a09967ad459c1305ef3f77653b47983a54
hansungj <43443895+hansungj@users.noreply.github.com>
2020-03-30 16:58:36 -0400

    Update Optimizer.py

--- Optimizer.py
+++ Optimizer.py
@@ -3,16 +3,27 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
 
+import os, copy, sys
+
 import numpy as np
 import random
 
 
 class SpecOptimizer:
+	'''
+	We backpropagate for 35 time steps using stochastic gradient descent
+	where the learning rate is initially set to 1.0 and halved if the 
+	perplexity does not decrease by more than 1.0 on the validation set 
+	after an epoch.
 
+	'''
 
-	def __init__(self, model, optimizer, initial_lr = 0.01, max_norm = 5):
+	def __init__(self, model, optimizer, initial_lr = 0.01, max_norm = 5, perplexity = None):
 		self.lr = initial_lr
-		self._perplexity = 1e12
+		if perplexity is None:
+			self._perplexity = 1e12
+		else:
+			self._perplexity = perplexity
 		self.optimizer = optimizer
 		self.max_norm = max_norm
 		self._step = 0
@@ -38,7 +49,9 @@ class SpecOptimizer:
 
 
 		self.optimizer.step()
-
+		
+	def zero_grad(self):
+		self.optimizer.zero_grad()
 
 	def update_lr(self, perplexity):
 
@@ -53,17 +66,28 @@ class SpecOptimizer:
 
 class LossComputer:
 
-	def __init__(self, optimizer, loss_fn):
+	def __init__(self, optimizer, loss_fn, jointly_train = False):
 		self.loss_fn = loss_fn 
 		self.optimizer = optimizer
+		self.jointly_train = jointly_train
 
 	def __call__(self, out, tgt):
 
-		loss = self.loss_fn(out.transpose(1, 2) ,tgt)
+		'''
+
+		if jointly trained, output from the model will be a tuple of output tensors
+		'''
+
+		if self.jointly_train:
+			loss = self.loss_fn(out[0].transpose(1,2),tgt)
+			loss += self.loss_fn(out[1].transpose(1,2),tgt)
+		else:
+			loss = self.loss_fn(out.transpose(1, 2) ,tgt)
+
 		loss.backward()
 
 		self.optimizer.step()
-		self.optimizer.optimizer.zero_grad()
+		self.optimizer.zero_grad()
 
 		return loss
 
@@ -71,13 +95,17 @@ class LossComputer:
 		self.optimizer.update_lr(perplexity)
 
 
-def run_epoch(data, val_data, model, loss_compute, epoch, verbose = 20):
+def run_epoch(data, val_data, model, loss_compute, epoch, verbose = 20, jointly_train = False):
 
 	total_loss = 0
 	model.train()
 	for i, (src, tgt) in enumerate(data):
-		out = model(src)
-		loss_one_step = loss_compute(out, tgt)
+		if jointly_train:
+			f_out, b_out = model(src)
+			loss_one_step = loss_compute((f_out,b_out), tgt)
+		else:
+			out = model(src)
+			loss_one_step = loss_compute(out, tgt)
 		total_loss += float(loss_one_step)
 
 		if i % verbose == 0:
@@ -89,12 +117,17 @@ def run_epoch(data, val_data, model, loss_compute, epoch, verbose = 20):
 	total_val_loss = 0
 	model.eval()
 	for i, (val_src, val_tgt) in enumerate(val_data):
-		val_out = model(val_src)
+		if jointly_train:
+			val_f_out, val_b_out = model(val_src)
+
+			val_loss = loss_compute.loss_fn(val_f_out.transpose(1, 2), val_tgt) 
+			val_loss += loss_compute.loss_fn(val_b_out.transpose(1, 2), val_tgt) 
+		else:
+			val_out = model(val_src)
+			val_loss = loss_compute.loss_fn(val_out.transpose(1, 2), val_tgt)
 
 		#note that perplexity is the inverse of negative log likelihood
-		val_loss = loss_compute.loss_fn(val_out.transpose(1, 2), val_tgt)
 		val_perplexity += float(torch.exp(val_loss))
-
 		total_val_loss += float(val_loss)
 	
 	total_val_loss /= i
